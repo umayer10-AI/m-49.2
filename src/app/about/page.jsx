cb4ffc563067ea96267809377379e763c405a6dc
umayer10-AI <mdumayer10@gmail.com>
2026-05-04 19:36:48 +0600

    ad

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-const page = () => {
+const page = async () => {
+
+    const res = await fetch("http://localhost:8000/about")
+    const data = await res.json()
+    console.log(data)
+
     return (
         <div>
             <h2 className='text-2xl font-bold'>About: </h2>
