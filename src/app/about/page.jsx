6544bdb4c3085af355d1d416a55e84b908c185d2
umayer10-AI <mdumayer10@gmail.com>
2026-05-04 19:31:31 +0600

    ad

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2 className='text-2xl font-bold'>About: </h2>
+        </div>
+    );
+};
+
+export default page;
